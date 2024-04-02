bb8f0dac40c2dd46905d0bcd34e7bbb703aa681b
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2024-04-02 16:06:18 -0500

    Clean up StageManager file

--- Assets/_Target/StageManager.cs
+++ Assets/_Target/StageManager.cs
@@ -8,29 +8,22 @@ using UnityEngine;
 
 namespace Guidance.Gameplay.Targets {
   public class StageManager : MonoBehaviour, IStageTransition {
-    private const float MINIMUM_X_SPAWN_LOCATION = -4.5F;
-    private const float MAXIMUM_X_SPAWN_LOCATION = 4.5f;
-
     private readonly float m_YSpawnDistance = Constants.Y_STAGE_LENGTH;
 
     public event Action OnTargetReached;
 
     [SerializeField] private GameObject m_TargetPrefab;
-    [SerializeField] private Target m_CurrentGoalTarget;
-    [SerializeField] private Target m_PreviousGoalTarget = null;
     [SerializeField] private GameObject m_ObstaclePrefab;
-    private StageData[] m_StageData;
-    private int m_StageNumber;
-
     private List<Target> m_Targets;
     private List<Obstacle> m_Obstacles;
+
+    private StageData[] m_StageData;
+    private int m_StageNumber;
 
     private void Awake() {
       m_Targets = new();
       m_Obstacles = new();
       LoadStageData();
-      //GameObject initialTarget = SpawnNewTarget(-2.97f, -5.5f, 0f);
-      //RegisterNewTarget(initialTarget);
     }
 
     public void DeactivatePreviousGoalTarget() {
@@ -66,8 +59,6 @@ namespace Guidance.Gameplay.Targets {
 
 
     private void TargetGoalLocation_OnTargetReached() {
-      //GameObject newTarget = SpawnNewTarget();
-      //RegisterNewTarget(newTarget);
       OnTargetReached?.Invoke();
     }
 
@@ -85,23 +76,6 @@ namespace Guidance.Gameplay.Targets {
         newObstacle.transform.localScale = new Vector3(obstacle.Scale, 1f, 1f);
         m_Obstacles.Add(newObstacle.GetComponent<Obstacle>());
       }
-    }
-
-    private GameObject SpawnNewTarget() {
-      Vector3 seedLocation = m_Targets[m_Targets.Count - 1].transform.position;
-      float spawnLocationX = UnityEngine.Random.Range(MINIMUM_X_SPAWN_LOCATION, MAXIMUM_X_SPAWN_LOCATION);
-      float spawnLocationY = seedLocation.y - m_YSpawnDistance;
-      float spawnLocationZ = seedLocation.z;
-      Vector3 spawnLocation = new Vector3(spawnLocationX, spawnLocationY, spawnLocationZ);
-      GameObject newTarget = Instantiate(m_TargetPrefab, spawnLocation, Quaternion.identity, transform);
-      return newTarget;
-    }
-
-    private GameObject SpawnNewTarget(float xPos, float yPos, float zPos) {
-      //float spawnLocationX = UnityEngine.Random.Range(MINIMUM_X_SPAWN_LOCATION, MAXIMUM_X_SPAWN_LOCATION);
-      Vector3 spawnLocation = new Vector3(xPos, yPos, zPos);
-      GameObject newTarget = Instantiate(m_TargetPrefab, spawnLocation, Quaternion.identity, transform);
-      return newTarget;
     }
 
     private GameObject SpawnNewTarget(Vector3 spawnPositon) {
