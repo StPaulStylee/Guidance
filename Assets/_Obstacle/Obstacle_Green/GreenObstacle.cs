6b4fdf039ae4867a5dc24eee9c62dda9e841472d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2024-04-29 15:16:44 -0500

    First 8 stages are ready

--- Assets/_Obstacle/Obstacle_Green/GreenObstacle.cs
+++ Assets/_Obstacle/Obstacle_Green/GreenObstacle.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 namespace Guidance.Gameplay.Obstacles {
   public class GreenObstacle : Obstacle, ICollisionBehavior {
     public void ApplyCollisionBehavior(Ball ball) {
-      Debug.Log("Applying Force");
       Vector3 direction = transform.right;
       ball.Rb.AddForce(direction * 0.2f, ForceMode.Impulse);
     }
