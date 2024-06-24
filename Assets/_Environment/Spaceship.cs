60ac023f84073c8532b10ec05d06577ab587c416
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2024-06-24 07:04:32 -0500

    Beginning to add spaceships to fly around in the background

---
+++ Assets/_Environment/Spaceship.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Guidance.Environment {
+  public class Spaceship : MonoBehaviour {
+    [SerializeField] private float m_Speed;
+    private Vector3 m_MovementVector;
+
+    private void Awake() {
+      m_MovementVector = new Vector3(0f, m_Speed, 0f);
+    }
+    void Update() {
+      transform.Translate(Vector3.left * m_Speed * Time.deltaTime, Space.World);
+    }
+  }
+}
