475c2c48aa0e844c655e91f56cb61bcb710bcce6
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2024-09-03 06:10:34 -0500

    First pass at emission pulse logic

--- Assets/_TitleScreen/TitleController.cs
+++ Assets/_TitleScreen/TitleController.cs
@@ -1,16 +1,53 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Guidance.Title {
   public class TitleController : MonoBehaviour {
     [SerializeField] private TitleMaterial_SO[] m_TitleMaterial_SOs;
     [SerializeField] private GameObject m_GuidanceText;
-    void Start() {
+    [SerializeField] float m_PulseDuration = .25f;
+    private Coroutine m_PulsingCoroutine;
+    private bool isPulsingActive = true;
+    //private float m_PulsePhaseDuration;
 
+    // Before I continue I need to make a copy of all the materials so I don't actually edit 
+    // the provided materials because those changes are permanent.
+
+    private void Awake() {
+      //m_PulsePhaseDuration = m_PulseDuration / 2;
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void Start() {
+      //m_PulsingCoroutine = StartCoroutine(PulseEmission());
+    }
 
+    private IEnumerator PulseEmission() {
+      while (isPulsingActive) {
+        foreach (var titleMaterial in m_TitleMaterial_SOs) {
+          yield return PulseToEmissionColor(titleMaterial.MaxEmissionColor);
+          yield return PulseToEmissionColor(titleMaterial.DefaultColor);
+        }
+      }
+    }
+
+    private IEnumerator PulseToEmissionColor(Color targetColor) {
+      foreach (var titleMaterial in m_TitleMaterial_SOs) {
+        Color currentEmission = titleMaterial.Material.GetColor("_EmissionColor");
+        float timeElapsed = 0f;
+        while (timeElapsed < m_PulseDuration) {
+          Color newEmissionColor = Color.Lerp(currentEmission, targetColor, timeElapsed / m_PulseDuration);
+          titleMaterial.Material.SetColor("_EmissionColor", newEmissionColor);
+          timeElapsed += Time.deltaTime;
+          yield return null;
+        }
+        titleMaterial.Material.SetColor("_EmissionColor", targetColor);
+      }
+    }
+
+    private void OnDisable() {
+      if (m_PulsingCoroutine != null) {
+        StopCoroutine(m_PulsingCoroutine);
+      }
     }
   }
 }
--- Assets/_TitleScreen/TitleMaterial_SO.cs
+++ Assets/_TitleScreen/TitleMaterial_SO.cs
@@ -6,6 +6,9 @@ namespace Guidance.Title {
   [CreateAssetMenu(fileName = "Title Material", menuName = "ScriptableObjects/Title Material", order = 2)]
 
   public class TitleMaterial_SO : ScriptableObject {
+    public Material Material => m_Material;
+    public Color DefaultColor => m_DefaultColor;
+    public Color MaxEmissionColor => m_MaxEmissionColor;
     [SerializeField] private Material m_Material;
     [SerializeField][ColorUsage(false, true)] private Color m_DefaultColor;
     [SerializeField][ColorUsage(false, true)] private Color m_MaxEmissionColor;
